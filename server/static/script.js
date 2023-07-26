3c40b272eff34ae04c4c01b19125213a78a00fe2
ParkMini <parkteammini@gmail.com>
2023-07-26 23:43:33 +0900

    함수 생성

---
+++ server/static/script.js
@@ -0,0 +1,42 @@
+function humanDetection() {
+  $.ajax({
+    url: "./humanDetection",
+    method: "GET",
+  });
+}
+
+function objectDetection() {
+  $.ajax({
+    url: "./objectDetection",
+    method: "GET",
+  });
+}
+
+function busNumDetection() {
+  $.ajax({
+    url: "./busNumDetection",
+    method: "GET",
+  });
+}
+
+function dotDetection() {
+  $.ajax({
+    url: "./dotDetection",
+    method: "GET",
+  });
+}
+
+function manyHumanDetection() {
+  $.ajax({
+    url: "./manyHumanDetection",
+    method: "GET",
+  });
+}
+
+function useDayDetection() {
+  $.ajax({
+    url: "./useDayDetection",
+    method: "GET",
+  });
+}
+
